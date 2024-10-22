a05638696abbc4e8d16b21e554b600c4fb17b625
Анастасия <131755126+Grycccry13@users.noreply.github.com>
2024-10-22 17:22:47 +0700

    Update send_message.sh

--- rabota5/send_message.sh
+++ rabota5/send_message.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Получаем данные от пользователя
-read -p "Введите сообщение: " SOME_TEXT
-read -p "Введите идентификатор чата: " CHAT_ID
-read -p "Введите токен бота: " BOT_TOKEN
+read -p "Введите сообщение: " Привет
+read -p "Введите идентификатор чата: " 123456789
+read -p "Введите токен бота: " 8071793757:AAFOgcf6UjcmtzOPK9PnOfjGdBUj6SQeGzQ
 
 # Формируем URL-запрос
 URL="https://api.telegram.org/bot$BOT_TOKEN/sendMessage?chat_id=$CHAT_ID&text=$SOME_TEXT"
